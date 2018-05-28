853308a8b3d98090c3295b3cd20ff0437560e9e0
Edmond Gagavo <deegavs@hotmail.com>
2018-05-28 13:01:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-frontend-nanodegree-arcade-game
-===============================
+# Frontend Nanodegree Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Still under construction!
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## Game Description
+
+This classic Arcade game is the 4th student project in the Front-end developer program.
+
+## How to Complete the Project.
+
+Provided with the game engine and additional materials from the `startup code`
+The required task is to complete the `app.js` file using object-oriented Javascript.
+
+## Instructions to Play the Game
+
+The goal is to move the figure at the bottom to cross the bug's highway to reach the water without colliding with the bugs passing by at random speeds.
+
+Use the arrow keys on your keyword to move the player figure.
+-**&uarr;** to go up
+-**&larr;** to go left
+-**&rarr;** to go right
+-**&darr;** to go down
+
+## Play the Game
+
+Play the game [here](https://github.com/deegavs/frontend-nanodegree-arcade-game)
--- js/app.js
+++ js/app.js
@@ -1,46 +1,132 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
+  if (this.x > 505) {
+    this.x = 0;
+    this.speed = 100 + Math.floor(Math.random() * 220);
+  };
+  // check for collisions with enemies and the player
+  if (player.x < this.x + 80 &&
+    player.x + 80 > this.x &&
+    player.y < this.y + 30 &&
+    30 + player.y > this.y) {
+    player.x = 202;
+    player.y = 404;
+    record.updateHits();
+  };
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class & character
+var Player = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.player = 'images/char-boy.png';
+}
+// This class requires an update(), render()
+Player.prototype.update = function(dt) {
+  return Player;
+}
+// Renders the player character
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.player), this.x, this.y);
+}
+// Player handleInput() method enables the player to move anywhere in within the x and y axis
+Player.prototype.handleInput = function(moveKey) {
+  if (moveKey == 'left' && this.x > 0) {
+    this.x -= 102;
+  }
+  if (moveKey == 'right' && this.x < 404) {
+    this.x += 102;
+  }
+  if (moveKey == 'up' && this.y > 0) {
+    this.y -= 83;
+  }
+  if (moveKey == 'down' && this.y < 404) {
+    this.y += 83;
+  }
+  if (this.y < 0) {
+    setTimeout(() => {
+      this.x = 202;
+      this.y = 404;
+      record.updateRuns();
+      record.updateScore();
+    }, 700);
 
+  }
+
+};
+
+//  Records the succesful runs and hits
+var Record = function() {
+  this.runs = 0;
+  this.hits = 0;
+  this.score = 0;
+};
+// Updates how many runs achieved
+Record.prototype.updateRuns = function() {
+  this.runs += 1;
+  document.getElementById('runs').innerHTML = this.runs;
+};
+// Updates the collisions encounted
+Record.prototype.updateHits = function() {
+  this.hits += 1;
+  document.getElementById('hits').innerHTML = this.hits;
+};
+// Updates the score as each time the reaches the water
+Record.prototype.updateScore = function(score) {
+  this.score += 100;
+  document.getElementById('points').innerHTML = this.score;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var enemyLocations = [60, 144, 227];
 
+enemyLocations.forEach(function(location) {
+  enemy = new Enemy(0, location, 200);
+  allEnemies.push(enemy);
+});
+// Starting location of the player on the x and y axis.
+var player = new Player(202, 404);
+var record = new Record();
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
